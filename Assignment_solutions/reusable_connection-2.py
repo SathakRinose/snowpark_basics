ea583572002fc0448008818a6c598b169a8c8d17
SathakRinose <sathakabdula3@gmail.com>
2026-06-30 21:39:18 -0700

    commit chhanges

--- Assignment_solutions/reusable_connection-2.py
+++ Assignment_solutions/reusable_connection-2.py
@@ -4,7 +4,8 @@ import snowflake.snowpark.functions
 from snowflake.snowpark import Session
 from snowflake.snowpark.functions import col
 sys.path.append("C:/Users/Hp/AppData/Local/Python/pythoncore-3.14-64/Lib")
-from generic_code import code_library   
+from generic_code import  code_library  
 connection_parameters = {"account":"TMRKOTV-GE73803","user":"SathakRinose","password":"Treselle@367$%","role":"ACCOUNTADMIN","warehouse":"COMPUTE_WH","database":"DEMO_DB","schema":"PUBLIC"}
-session = code_library.create_session(connection_parameters)
 
+
+session=code_library.snowconnection(connection_parameters)
